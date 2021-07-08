2c1b6533967556a15f5f23320dfdeb3212a8e3c1
aaa129 <b1802067@planet.kanazawa-it.ac.jp>
2021-07-08 23:19:47 +0900

    PHP_Update

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -12,13 +12,43 @@ use App\Http\Requests\HelloRequest;
 class HelloController extends Controller
 {
     public function index(Request $request){
-        if(isset($request->id)){
-            $param = ["id" => $request->id];
-            $items = DB::select("select * from preople where id = :id",$param);
-        } else {
-            $items = DB::select("select * from preople");
-        }
-       return view('hello.index',['items'=>$items]);
+        $items = DB::select("select * from preople");
+        return view('hello.index',['items'=>$items]);
     }
 
+    public function post(Request $request){
+        $items = DB::select("select * from preople");
+        return view('hello.index',['items'=>$items]);
+    }
+
+    public function add(Request $request){
+        return view("hello.add");
+    }
+
+    public function create(Request $request){
+        $param = [
+            "name" => $request->name,
+            "mail" => $request->mail,
+            "age" => $request -> age,
+        ];
+        DB::insert("insert into preople (name,mail,age) values(:name,:mail,:age)",$param);
+        return redirect("/hello");
+    }
+
+    public function edit(Request $request){
+        $param = ["id" => $request -> id];
+        $item = DB::select("select * from preople where id = :id",$param);
+        return view("hello.edit",["form" => $item[0]]);
+    }
+
+    public function update(Request $request){
+        $param = [
+            "id" => $request->id,
+            "name" => $request->name,
+            "mail" => $request->mail,
+            "age" => $request->age,
+        ];
+        DB::update("update preople set name = :name,mail = :mail,age = :age where id = :id",$param);
+        return redirect("/hello");
+    }
 }
--- laravelapp/routes/web.php
+++ laravelapp/routes/web.php
@@ -16,10 +16,10 @@ use App\Http\Middleware\HelloMiddleware;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get("info",function(){
-    return view("hello.info");
-});
 Route::get("hello","HelloController@index");
-//Route::post("hello","HelloController@post");
+Route::get("hello/add","HelloController@add");
+Route::post("hello/add","HelloController@create");
+Route::get("hello/edit","HelloController@edit");
+Route::post("hello/edit","HelloController@update");
 
 
